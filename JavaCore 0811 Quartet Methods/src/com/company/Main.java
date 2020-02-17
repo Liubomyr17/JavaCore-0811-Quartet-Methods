a6d69f0f2bb24c1c37eba9e07d2e7e56a4a4fbfe
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-17 10:37:57 +0200

    Add files via upload

---
+++ JavaCore 0811 Quartet Methods/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+/*
+
+0811 Quartet Methods
+
+Implement 4 methods.
+Each of them should return a list that is best suited for performing these operations (it will cope with a large number of operations most quickly).
+Nothing to measure.
+Requirements:
+1. The program should not display text on the screen.
+2. The Solution class should contain only 5 methods.
+3. The getListForGet method should return the list that is best suited for the pick operation.
+4. The getListForSet method should return the list that is best for setting the value of an element.
+5. The getListForAddOrInsert method should return the list that is best for adding or inserting an item.
+6. The getListForRemove method should return the list that is best suited for the delete operation of an element.
+
+
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.util.*;
+
+
+public class Main {
+    public static List getListForGet() {
+        return new ArrayList();
+    }
+    public static List getListForSet() {
+        return new ArrayList();
+    }
+    public static List getListForInsert() {
+        return new LinkedList();
+    }
+    public static List getListForRemove() {
+        return new LinkedList();
+    }
+    public static void main(String[] args) throws Exception {
+    }
+}
+
+
+
+
